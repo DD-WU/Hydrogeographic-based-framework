106d611109fbb8024ab5781c20ae8a1cfeb69e1a
Wanghaocheng <1585978774@qq.com>
2024-05-28 18:10:54 +0800

    pull2

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -10,18 +10,18 @@
 using namespace std::chrono;
 int main()
 {
-	//////case A/case 1
-	//new test1("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test1");
-	////case B
-	//new test2("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test2");
-	////case C
-	//new test3("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test3");
-	////case D
-	//new test4("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test4");
-	////case E/case 4
-	//new test5("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test5");
-	////case 3
-	//new test6("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test6");
+	////case A/case 1
+	new test1("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test1");
+	//case B
+	new test2("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test2");
+	//case C
+	new test3("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test3");
+	//case D
+	new test4("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test4");
+	//case E/case 4
+	new test5("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test5");
+	//case 3
+	new test6("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test6");
 	//case 2
 	new test7("F:\\SWMM_lisflood\\simulation\\parameter.xls", "test7");
 }
